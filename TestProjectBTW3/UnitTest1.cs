a08cb7a82f8e509edcf1e3ea1abdfc4c1ea49e87
MARLON MURCIA BETANCOURT <desarrollogithub@bythewave.co>
2023-04-21 08:10:41 -0500

    Exponente de un numero n

--- TestProjectBTW3/UnitTest1.cs
+++ TestProjectBTW3/UnitTest1.cs
@@ -24,12 +24,27 @@ namespace TestProjectBTW3
             modelo.Operacion = "+";
 
             var respuesta =  await controlador.Enter(modelo);
-
-            
-
+           
             var respuesta3 = (CalculadoraViewModel)(((ViewResult)respuesta).Model);
 
             Assert.That(respuesta3.Resultado,Is.EqualTo(6));
+        }
+
+        [Test]
+        public async Task ExponencialMarlonM77()
+        {
+
+            var controlador = new CalculadoraController();
+            var modelo = new CalculadoraViewModel();
+
+            modelo.Numero1 = 1;
+            modelo.Numero2 = 2;
+            modelo.Operacion = "^";
+
+            var response = await controlador.Enter(modelo);
+            var response3 = (CalculadoraViewModel)(((ViewResult)response).Model);
+
+            Assert.That(response3.Resultado, Is.EqualTo(1));
         }
     }
 }
